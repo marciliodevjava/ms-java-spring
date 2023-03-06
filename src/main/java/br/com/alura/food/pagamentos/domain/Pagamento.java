32b66e173a5d12d2bb31871c3303327e8b509db2
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-06 09:21:38 -0300

    Criação do Domain Pagamento

--- src/main/java/br/com/alura/food/pagamentos/domain/Pagamento.java
+++ src/main/java/br/com/alura/food/pagamentos/domain/Pagamento.java
@@ -1,11 +1,17 @@
 package br.com.alura.food.pagamentos.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import br.com.alura.food.pagamentos.domain.enuns.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "pagamentos")
@@ -14,5 +20,32 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Positive
+    private BigDecimal valor;
+    @NotBlank
+    @Size(min = 10, max = 150)
+    private String nome;
+    @NotBlank
+    @Size(max = 19)
+    private String numero;
+    @NotBlank
+    @Size(max = 7)
+    private String expiracao;
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String codigo;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @NotNull
+    private Long pedidoId;
+    @NotNull
+    private Long formaDePagamentoId;
+
 
 }
