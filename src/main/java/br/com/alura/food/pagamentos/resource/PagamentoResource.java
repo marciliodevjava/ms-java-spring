bed3a1b65efea0051652f7bb4c36008cdfba1c5b
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-07 15:18:14 -0300

    Atualização

--- src/main/java/br/com/alura/food/pagamentos/resource/PagamentoResource.java
+++ src/main/java/br/com/alura/food/pagamentos/resource/PagamentoResource.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
