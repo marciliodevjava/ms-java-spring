3af32ee787b4eb9f92df5c6fa6cee09c125b2025
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-10 20:42:44 -0300

    Correção de imports PagamentosApplication

--- src/main/java/br/com/alura/food/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/alura/food/pagamentos/PagamentosApplication.java
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
