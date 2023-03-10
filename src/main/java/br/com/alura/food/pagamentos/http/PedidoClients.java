1bd087fbe9a39520738f9a99da45c87c0c48c961
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-03-10 20:42:26 -0300

    Correção de imports PedidoClients

--- src/main/java/br/com/alura/food/pagamentos/http/PedidoClients.java
+++ src/main/java/br/com/alura/food/pagamentos/http/PedidoClients.java
@@ -1,8 +1,6 @@
 package br.com.alura.food.pagamentos.http;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
